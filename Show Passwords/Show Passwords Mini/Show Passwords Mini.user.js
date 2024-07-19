f7a7564740dc323a24aa5120330d0b2dcf4b3903
Amir <soheyl637@gmai.com>
2024-07-20 00:35:54 +0100

    docs(Show Passwords Mini): Update script metadata, replace @url with @homepage and add @downloadURL

--- Show Passwords/Show Passwords Mini/Show Passwords Mini.user.js
+++ Show Passwords/Show Passwords Mini/Show Passwords Mini.user.js
@@ -3,12 +3,13 @@
 // @version      0.1.1
 // @description  Reveals the password when hovering the mouse pointer over a password field.
 // @author       Amm1rr
-// @url          https://github.com/Amm1rr/Userscripts/tree/main/Show%20Passwords/Show%20Passwords%20Mini
+// @homepage     https://github.com/Amm1rr/Userscripts/tree/main/Show%20Passwords/Show%20Passwords%20Mini/
 // @icon         https://cdn.jsdelivr.net/gh/Amm1rr/Userscripts@main/Show%20Passwords/Show%20Passwords%20Mini/Show%20Passwords%20Mini.icon.png
 // @match        *://*/*
 // @license      MIT
 // @namespace    amm1rr.Userscripts.show.passwords.mini
 // @updateURL    https://github.com/Amm1rr/Userscripts/raw/main/Show%20Passwords/Show%20Passwords%20Mini/Show%20Passwords%20Mini.user.js
+// @downloadURL  https://github.com/Amm1rr/Userscripts/raw/main/Show%20Passwords/Show%20Passwords%20Mini/Show%20Passwords%20Mini.user.js
 // ==/UserScript==
 
 (function() {
